4bb9e50f39ba9cee5dee661ffd121a3b79c77423
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-04 10:35:36 -0700

    Updates

---
+++ precache-manifest.b44091dd5545ca2b824256978dfccf1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda20763a18203f1fdec8f2da4a3b4cb",
+    "url": "/React-calculator/index.html"
+  },
+  {
+    "revision": "0092b2c21ba2d4c5cfb5",
+    "url": "/React-calculator/static/css/main.4d234234.chunk.css"
+  },
+  {
+    "revision": "81c8b17fdd643ee28cbd",
+    "url": "/React-calculator/static/js/2.b29f4236.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-calculator/static/js/2.b29f4236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0092b2c21ba2d4c5cfb5",
+    "url": "/React-calculator/static/js/main.fda6c7aa.chunk.js"
+  },
+  {
+    "revision": "436eec30c65cc1dd148b",
+    "url": "/React-calculator/static/js/runtime-main.f6b6bc55.js"
+  }
+]);
